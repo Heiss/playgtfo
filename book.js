534be0e80e48c2e8cd5e396b94c4cda3d29b36a3
Heiss <Heiss@users.noreply.github.com>
2022-01-03 14:51:19 +0000

    deploy: 0fa56f5a046647c32866a2c3dae8b41dcb091f70

--- book.js
+++ book.js
@@ -169,9 +169,9 @@ function playground_text(playground) {
         Array
         code_nodes
             .filter(function (node) {return !node.classList.contains("editable"); })
-            .forEach(function (block) { hljs.highlightBlock(block); });
+            .forEach(function (block) { hljs.highlightElement(block); });
     } else {
-        code_nodes.forEach(function (block) { hljs.highlightBlock(block); });
+        code_nodes.forEach(function (block) { hljs.highlightElement(block); });
     }
 
     // Adding the hljs class gives code blocks the color css
@@ -371,14 +371,7 @@ function playground_text(playground) {
     });
 
     themePopup.addEventListener('click', function (e) {
-        var theme;
-        if (e.target.className === "theme") {
-            theme = e.target.id;
-        } else if (e.target.parentElement.className === "theme") {
-            theme = e.target.parentElement.id;
-        } else {
-            return;
-        }
+        var theme = e.target.id || e.target.parentElement.id;
         set_theme(theme);
     });
 
